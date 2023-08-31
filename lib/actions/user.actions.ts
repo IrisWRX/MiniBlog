742cb2abddb4a45973cfcbe5c345cc50deaf166e
IrisWRX <wxu64@my.bcit.ca>
2023-08-31 12:28:05 -0700

    added profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -45,5 +46,33 @@ export async function fetchUser(userId: string) {
     return await User.findOne({ id: userId });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // 'threads' will be a User document with the 'threads' field populated with Thread documents.
+    // Each Thread document's 'children' field will be populated with Thread documents,
+    // and the 'author' field of those child Thread documents will be populated with User documents.
+    const threads = await User.findOne({ id: userId }).populate({
+      // 'path' specifies the field to populate
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
