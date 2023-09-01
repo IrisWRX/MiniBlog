2488165aea66413732b3107f4c9823cd276f7050
IrisWRX <wxu64@my.bcit.ca>
2023-09-01 16:40:51 -0700

    removed right sidebar, and heart, share, repost

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -69,13 +69,6 @@ const ThreadCard = ({
 
             <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className="flex gap-3.5">
-                <Image
-                  src="/assets/heart-gray.svg"
-                  alt="heart"
-                  width={24}
-                  height={24}
-                  className="cursor-pointer object-contain"
-                />
                 <Link href={`/thread/${id}`}>
                   <Image
                     src="/assets/reply.svg"
@@ -85,20 +78,6 @@ const ThreadCard = ({
                     className="cursor-pointer object-contain"
                   />
                 </Link>
-                <Image
-                  src="/assets/repost.svg"
-                  alt="repost"
-                  width={24}
-                  height={24}
-                  className="cursor-pointer object-contain"
-                />
-                <Image
-                  src="/assets/share.svg"
-                  alt="share"
-                  width={24}
-                  height={24}
-                  className="cursor-pointer object-contain"
-                />
               </div>
 
               {isComment && comments.length > 0 && (
